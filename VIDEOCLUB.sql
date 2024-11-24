1042208127b087986041b186896dd6ddf8640137
CarlitooooK <carlocabrera8485@gmail.com>
2024-11-23 21:29:43 -0600

    Etapa Final

--- VIDEOCLUB.sql
+++ VIDEOCLUB.sql
@@ -1,7 +1,7 @@
  CREATE DATABASE VIDEOCLUB
     USE VIDEOCLUB
-    USE master
     
+DROP DATABASE VIDEOCLUB
 
 --CREACION TABLAS
 CREATE TABLE Cliente(
@@ -21,8 +21,6 @@ CREATE TABLE Cliente(
 )
 
 --DBCC CHECKIDENT ('nombre_tabla', RESEED, nueva_semilla);
-
-
 CREATE TABLE Membresia(
     ID_Membresia int primary key identity(10,1),
     tipo varchar(30),
@@ -37,8 +35,6 @@ CREATE TABLE Proveedor(
     tipo varchar(10)
 )
 
-
-
 CREATE TABLE Empleado(
 	ID_Empleado int primary key identity(10,1),
 	nombre varchar(30),
@@ -51,8 +47,8 @@ CREATE TABLE Alquiler(
 	fecha_alquiler DATE,
 	fecha_devolucion DATE,
 	monto AS (DATEDIFF(DAY,fecha_alquiler,fecha_devolucion)*12.0) PERSISTED,
-    ID_Cliente int,
-    ID_Empleado int
+    ID_Cliente INT,
+    ID_Empleado INT
 )
 
 CREATE TABLE Pelicula(
@@ -69,7 +65,7 @@ CREATE TABLE Inventario(
     ID_Inventario int PRIMARY KEY identity(10,1),
 	fecha_adquisicion DATE,
     estado varchar(15),
-    ID_Pelicula int	
+    ID_Pelicula INT
 )
 
 CREATE TABLE Alquiler_Inventario(
@@ -100,8 +96,6 @@ ALTER TABLE Empleado
  ALTER TABLE Cliente
     ADD CONSTRAINT CK_Nombre CHECK(nombre NOT LIKE '%[^A-Za-z ]%'); --Nombre solo letras min y mayus
 
-
-
 ALTER TABLE Cliente
     ADD CONSTRAINT CK_Contacto CHECK(contacto NOT LIKE '%[^0-9]%') --Contacto solo numeros(cel)
 
@@ -122,8 +116,7 @@ ALTER TABLE Proveedor
         email LIKE '%_@gmail.es' OR
         email LIKE '%_@yahoo.es' OR
         email LIKE '%_@outlook.es'
-    )
-	
+    )	
 --Restricciones para Alquiler
 ALTER TABLE Alquiler
     ADD CONSTRAINT CK_FechaAlquiler CHECK(fecha_alquiler<=GETDATE())  --fecha real para alquiler
@@ -162,8 +155,6 @@ ALTER TABLE Cliente_Membresia
 ALTER TABLE Cliente_Membresia
     ADD FOREIGN KEY (ID_Cliente) REFERENCES Cliente(ID_Cliente)
 
-SELECT * FROM Cliente
-
 --AGREGAR VALORES
 INSERT INTO Cliente (nombre, email, contacto, direccion, fecha_nacimiento) VALUES
 ('Carlos Perez', 'carlos.perez@gmail.com', '5512345678', 'Av. Central 123', '1995-05-12'),
@@ -182,23 +173,11 @@ INSERT INTO Cliente (nombre, email, contacto, direccion, fecha_nacimiento) VALUE
 ('Sebastian Vega', 'sebastian.vega@gmail.es', '5543218765', 'Calle Brisa 654', '2002-03-21'),
 ('Natalia Ruiz', 'natalia.ruiz@gmail.com', '5576543219', 'Av. Rayo 987', '1987-10-18');
 
-SELECT * FROM Membresia
 INSERT INTO Membresia (tipo, descuento, costo_mensual) VALUES
 ('Basica', 10, 99.99),
 ('VIP', 20, 199.99),
 ('Premium', 30, 299.99);
 
-DELETE FROM Proveedor
-DELETE FROM Empleado
-DELETE FROM Inventario
-DELETE FROM Alquiler
-DELETE FROM Pelicula
-DELETE FROM Alquiler_Inventario
-DELETE FROM Proveedor_Pelicula
-DELETE FROM Cliente_Membresia
-
-
-SELECT* FROM Proveedor
 INSERT INTO Proveedor (nombre, email, tipo) VALUES
 ('Carlos Perez', 'carlos.perez@gmail.com', 'Peliculas'),
 ('Maria Lopez', 'maria.lopez@yahoo.com', 'Series'),
@@ -216,8 +195,6 @@ INSERT INTO Proveedor (nombre, email, tipo) VALUES
 ('Sebastian Vega', 'sebastian.vega@gmail.es', 'Series'),
 ('Natalia Ruiz', 'natalia.ruiz@gmail.com', 'Documnts');
 
-
-SELECT* FROM Empleado
 INSERT INTO Empleado (nombre, cargo, contacto) VALUES
 ('Luis Gomez', 'Gerente', '5532145678'),
 ('Ana Perez', 'Cajero', '5545678932'),
@@ -225,55 +202,16 @@ INSERT INTO Empleado (nombre, cargo, contacto) VALUES
 ('Juan Sanchez', 'Supervisor', '5523456789'),
 ('Carla Fernandez', 'Encargado', '5545678943'),
 ('Diego Ramirez', 'Cajero', '5556789210'),
-('Laura Alvarez', 'Supervisor', '5549876543'),
-('Pedro Vega', 'Gerente', '5521123456'),
-('Sofia Morales', 'Administrador', '5543219876'),
-('Fernando Lopez', 'Encargado', '5567894321'),
-('Miguel Herrera', 'Supervisor', '5523456123'),
+('Laura Alvarez', 'Ventas', '5549876543'),
+('Pedro Vega', 'Ventas', '5521123456'),
+('Sofia Morales', 'Ventas', '5543219876'),
+('Fernando Lopez', 'Ventas', '5567894321'),
+('Miguel Herrera', 'Ventas', '5523456123'),
 ('Natalia Garcia', 'Cajero', '5565432198'),
 ('Sebastian Cruz', 'Administrador', '5545678910'),
-('Valeria Ruiz', 'Encargado', '5551234567'),
+('Valeria Ruiz', 'Ventas', '5551234567'),
 ('Gloria Dominguez', 'Cajero', '5532147896');
 
-SELECT* FROM Inventario
-INSERT INTO Inventario (fecha_adquisicion, estado, ID_Pelicula) VALUES
-('2024-01-10', 'Disponible', 10),
-('2024-02-15', 'Disponible', 11),
-('2024-03-20', 'Rentado', 12),
-('2024-04-05', 'Rentado', 13),
-('2024-05-10', 'En Reparación', 14),
-('2024-06-25', 'Disponible', 15),
-('2024-07-14', 'Disponible', 16),
-('2024-08-01', 'Rentado', 17),
-('2024-09-18', 'Rentado', 18),
-('2024-10-22', 'En Reparación', 19),
-('2024-11-05', 'Disponible', 20),
-('2024-11-12', 'Disponible', 21),
-('2024-11-17', 'Rentado', 22),
-('2024-11-18', 'Rentado', 23),
-('2024-11-19', 'Disponible', 24);
-
-
-SELECT * FROM Alquiler
-INSERT INTO Alquiler (fecha_alquiler, fecha_devolucion, ID_Cliente, ID_Empleado) VALUES
-('2024-11-01', '2024-11-05', 10, 10),
-('2024-11-02', '2024-11-06', 11, 11),
-('2024-11-03', '2024-11-07', 12, 12),
-('2024-11-04', '2024-11-08', 13, 13),
-('2024-11-05', '2024-11-09', 14, 14),
-('2024-11-06', '2024-11-10', 15, 15),
-('2024-11-07', '2024-11-11', 16, 10),
-('2024-11-08', '2024-11-12', 17, 11),
-('2024-11-09', '2024-11-13', 18, 12),
-('2024-11-10', '2024-11-14', 19, 13),
-('2024-11-11', '2024-11-15', 20, 14),
-('2024-11-12', '2024-11-16', 21, 15),
-('2024-11-13', '2024-11-17', 22, 10),
-('2024-11-14', '2024-11-18', 23, 11),
-('2024-11-15', '2024-11-19', 24, 12);
-
--- Tabla Pelicula
-SELECT* FROM Pelicula
 INSERT INTO Pelicula (titulo, genero, duracion, año_lanzamiento, clasificacion, director) VALUES
 ('El Padrino', 'Drama/Crimen', '02:55:00', '1972-03-24', 'R', 'Francis Coppola'),
 ('El Señor Anillos', 'Fantasía/Aventura', '03:48:00', '2001-12-19', 'PG-13', 'Peter Jackson'),
@@ -291,8 +229,41 @@ INSERT INTO Pelicula (titulo, genero, duracion, año_lanzamiento, clasificacion,
 ('Toy Story', 'Animación/Familia', '01:21:00', '1995-11-22', 'G', 'John Lasseter'),
 ('Avatar', 'Ciencia Fic./Aventura', '02:42:00', '2009-12-18', 'PG-13', 'James Cameron');
 
-SELECT * FROM Alquiler_Inventario
-INSERT INTO Alquiler_Inventario (ID_Alquiler, ID_Inventario, cantidad) VALUES
+INSERT INTO Alquiler (fecha_alquiler, fecha_devolucion, ID_Cliente, ID_Empleado) VALUES
+('2024-11-01', '2024-11-10', 10, 11),
+('2024-11-02', '2024-11-06', 11, 11),
+('2024-11-03', '2024-11-22', 12, 15),
+('2024-11-04', '2024-11-06', 13, 21),
+('2024-11-05', '2024-11-11', 14, 16),
+('2024-11-11', '2024-11-23', 15, 17),
+('2024-11-02', '2024-11-11', 16, 18),
+('2024-11-08', '2024-11-12', 17, 19),
+('2024-11-09', '2024-11-13', 18, 20),
+('2024-11-10', '2024-11-11', 19, 20),
+('2024-11-21', '2024-11-22', 20, 23),
+('2024-11-01', '2024-11-16', 21, 11),
+('2024-11-01', '2024-11-17', 22, 24),
+('2024-11-02', '2024-11-18', 23, 16),
+('2024-11-15', '2024-11-19', 24, 11);
+
+INSERT INTO Inventario (fecha_adquisicion, estado, ID_Pelicula) VALUES  
+('2024-01-10', 'Disponible', 10),
+('2024-02-15', 'Disponible', 11),
+('2024-03-20', 'Rentado', 12),
+('2024-04-05', 'Rentado', 13),
+('2024-05-10', 'En Reparación', 14),
+('2024-06-25', 'Disponible', 15),
+('2024-07-14', 'Disponible', 16),
+('2024-08-01', 'Rentado', 17),
+('2024-09-18', 'Rentado', 18),
+('2024-10-22', 'En Reparación', 19),
+('2024-11-05', 'Disponible', 20),
+('2024-11-12', 'Disponible', 21),
+('2024-11-17', 'Rentado', 22),
+('2024-11-18', 'Rentado', 23),
+('2024-11-19', 'Disponible', 24);
+
+INSERT INTO Alquiler_Inventario (ID_Alquiler, ID_Inventario, cantidad) VALUES 
 (10, 10, 1),
 (11, 11, 1),
 (12, 12, 1),
@@ -309,7 +280,6 @@ INSERT INTO Alquiler_Inventario (ID_Alquiler, ID_Inventario, cantidad) VALUES
 (23, 23, 1),
 (24, 24, 1);
 
-SELECT * FROM Proveedor_Pelicula
 INSERT INTO Proveedor_Pelicula (ID_Proveedor, ID_Pelicula, precio, fecha_proveedor) VALUES
 (10, 10, 120.00, '2024-01-10'),
 (11, 11, 130.00, '2024-02-15'),
@@ -327,24 +297,82 @@ INSERT INTO Proveedor_Pelicula (ID_Proveedor, ID_Pelicula, precio, fecha_proveed
 (23, 23, 250.00, '2024-11-18'),
 (24, 24, 260.00, '2024-11-19');
 
-
-
-SELECT * FROM Cliente_Membresia
 INSERT INTO Cliente_Membresia (ID_Membresia, ID_Cliente, codigo_barras) VALUES
 (10, 10, '123456789012345'),
-(11, 11, '123456789112345'),
-(12, 12, '123456789212345'),
+(10, 11, '123456789112345'),
+(10, 12, '123456789212345'),
 (10, 13, '123456789312345'),
 (11, 14, '123456789412345'),
 (12, 15, '123456789512345'),
 (10, 16, '123456789612345'),
 (11, 17, '123456789712345'),
-(12, 18, '123456789812345'),
-(10, 19, '123456789912345'),
-(11, 20, '123456789013245'),
-(12, 21, '123456789113245'),
+(11, 18, '123456789812345'),
+(12, 19, '123456789912345'),
+(12, 20, '123456789013245'),
+(11, 21, '123456789113245'),
 (10, 22, '123456789213245'),
-(11, 23, '123456789313245'),
-(12, 24, '123456789413245');
+(10, 23, '123456789313245'),
+(10, 24, '123456789413245');
+
+SELECT * FROM Cliente
+SELECT * FROM Cliente_Membresia
+SELECT * FROM Membresia
+SELECT * FROM Empleado
+SELECT * FROM Proveedor
+SELECT * FROM Proveedor_Pelicula
+SELECT * FROM Alquiler
+SELECT * FROM Alquiler_Inventario
+SELECT * FROM Inventario
+SELECT * FROM Pelicula
 
 --CONSULTAS
+-- Consultar el promedio de duracion de las peliculas
+    SELECT CONVERT(VARCHAR, DATEADD(SECOND, AVG(DATEDIFF(SECOND, '00:00:00', duracion)), '00:00:00'), 108) AS 'DURACION PROMEDIO DE LAS PELICULAS'
+    FROM Pelicula
+
+--Consulta para encontrar el empleado con el mayor número de alquileres en el mes
+	SELECT TOP 1 nombre AS NOMBRE, COUNT(Alquiler.ID_Alquiler)as TOTAL_ALQUILERES 
+		FROM Empleado RIGHT JOIN Alquiler ON Alquiler.ID_Empleado = Empleado.ID_Empleado GROUP BY Empleado.nombre ORDER BY TOTAL_ALQUILERES DESC
+
+--Consultar cuál es la membresía menos adquirida
+	SELECT tipo, COUNT(Cliente_Membresia.ID_Cliente) AS MENOS_ADQUIRIDA 
+		FROM Membresia LEFT JOIN Cliente_Membresia ON Cliente_Membresia.ID_Membresia = Membresia.ID_Membresia GROUP BY tipo 
+			HAVING COUNT(Cliente_Membresia.ID_Cliente) = 
+				(SELECT MIN(MENOS_ADQUIRIDA) FROM (SELECT COUNT(Cliente_Membresia.ID_Cliente) AS MENOS_ADQUIRIDA 
+					FROM Membresia LEFT JOIN Cliente_Membresia ON Cliente_Membresia.ID_Membresia = Membresia.ID_Membresia GROUP BY tipo) AS ConteoMembresias)
+
+--Consultar los distintos géneros de las películas
+	SELECT DISTINCT genero AS GENEROS_PELICULAS FROM Pelicula
+
+--Consultar la fechas de duración del alquiler
+	SELECT ID_Alquiler, fecha_alquiler, fecha_devolucion, DATEDIFF(DAY, fecha_alquiler, fecha_devolucion) AS duracion_dias FROM Alquiler
+    
+--Consulta para obtener el total de ingresos en un mes específico
+	DECLARE @Mes INT = 11; -- Noviembre 
+	DECLARE @Año INT = 2024; -- Año 2024
+
+	SELECT SUM(monto) AS INGRESOS_MES FROM Alquiler WHERE MONTH(fecha_alquiler) = @Mes AND YEAR(fecha_alquiler) = @Año
+--FALTA UNA CONSULTA DE VARIABLES TEMPORALES !!
+
+
+--Consulta para mostrar todas las películas que aún no han sido alquiladas
+SELECT titulo FROM Pelicula LEFT JOIN Inventario ON Pelicula.ID_Pelicula = Inventario.ID_Pelicula LEFT JOIN Alquiler_Inventario ON Inventario.ID_Inventario = Alquiler_Inventario.ID_Inventario
+	WHERE Inventario.estado = 'Disponible' OR Inventario.estado = 'En Reparación'
+    
+--Consultar los nombres de los empleados con los titulos de las películas que han alquiladoo
+	SELECT E.nombre, P.titulo FROM Empleado E
+			INNER JOIN Alquiler ON E.ID_Empleado = Alquiler.ID_Empleado
+			INNER JOIN Alquiler_Inventario AlqIn ON Alquiler.ID_Alquiler = AlqIn.ID_Alquiler
+			INNER JOIN Inventario I ON AlqIn.ID_Inventario = I.ID_Inventario
+			INNER JOIN Pelicula P ON I.ID_Pelicula = P.ID_Pelicula
+				GROUP BY E.nombre, P.titulo
+
+-- Consultar los proveedores y las peliculas que han suministrado
+	SELECT Pr.nombre AS PROVEDOR , P.titulo AS PELICULA_SUMINISTRADA FROM Proveedor Pr
+	RIGHT JOIN Proveedor_Pelicula PP ON Pr.ID_Proveedor = PP.ID_Proveedor
+	RIGHT JOIN Pelicula P ON PP.ID_Pelicula = P.ID_Pelicula
+
+-- Consultar todos los clientes y sus membresias
+	SELECT Cliente.nombre AS CLIENTE, Membresia.tipo AS MEMBRESIA FROM Cliente
+	FULL OUTER JOIN Cliente_Membresia ON Cliente.ID_Cliente = Cliente_Membresia.ID_Cliente
+	FULL OUTER JOIN Membresia ON Cliente_Membresia.ID_Membresia = Membresia.ID_Membresia
